5ef9f0cda6554acd5b25f4e5f201e98c8b62dba9
Daniel Alvarez <daniel@alvarez.tech>
2020-11-11 22:53:56 -0400

    Topics

--- app/src/main/java/tech/alvarez/notifications/MainActivity.kt
+++ app/src/main/java/tech/alvarez/notifications/MainActivity.kt
@@ -3,16 +3,45 @@ package tech.alvarez.notifications
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.huawei.agconnect.config.AGConnectServicesConfig
 import com.huawei.hms.aaid.HmsInstanceId
-import tech.alvarez.notifications.MainActivity
+import com.huawei.hms.push.HmsMessaging
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.subscribeButton).setOnClickListener {
+            subscribe("topic1")
+        }
+        findViewById<Button>(R.id.unsubscribeButton).setOnClickListener {
+            unsubscribe("topic1")
+        }
+
         getToken()
+    }
+
+    private fun subscribe(topic: String) {
+        HmsMessaging.getInstance(this).subscribe(topic).addOnCompleteListener {
+            if (it.isSuccessful) {
+                Log.d(TAG, "Subscribe: SUCCESS")
+            } else {
+                Log.d(TAG, "Subscribe: FAILURE")
+            }
+        }
+    }
+
+    private fun unsubscribe(topic: String) {
+        HmsMessaging.getInstance(this).unsubscribe(topic).addOnCompleteListener {
+            if (it.isSuccessful) {
+                Log.d(TAG, "Unsubscribe: SUCCESS")
+            } else {
+                Log.d(TAG, "Unsubscribe: FAILURE")
+            }
+        }
     }
 
     private fun getToken() {
